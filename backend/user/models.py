33c3d2e79120d49dabb7ddc6d974bea59fa43276
leventepap <pap.levente.i@gmail.com>
2022-10-31 15:05:09 +0100

    fixing project folder

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return f'ID {self.id} : {self.username}'
