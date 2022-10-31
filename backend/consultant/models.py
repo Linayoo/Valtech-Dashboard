d67e5b56f33dda5cee404e389eb854cdb1d64d5c
Miri Yoo <violinmiriu@gmail.com>
2022-10-31 16:53:15 +0100

    Consultant model, Consultant admin added

--- backend/consultant/models.py
+++ backend/consultant/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Consultant(models.Model):
+    OFFICES = [
+        ('Baden', 'Baden'),
+        ('Bern', 'Bern'),
+        ('Geneva', 'Geneva')
+    ]
+
+    ROLES = [
+        ('frontend', 'Frontend Developer'),
+        ('backend', 'Backend Developer'),
+        ('devops', 'DevOps Developer'),
+        ('fullstack', 'Fullstack Developer')
+    ]
+
+    display_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    office_category = models.CharField(max_length=100, choices=OFFICES)
+    email = models.EmailField(unique=False)
+    linked_in_link = models.URLField(max_length=200)
+    image_path = models.FileField(upload_to='uploads/', blank=True)
+    summary = models.TextField()
+    is_subcontractor = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_disabled = models.BooleanField(default=False)
+    primary_language = models.CharField(max_length=100)
+    managed_certificates = models.CharField(max_length=250)
+    title = models.CharField(max_length=200)
+    edited_time = models.DateTimeField(auto_now=True)
+    manager_display_name = models.CharField(max_length=100)
+    profile_id = models.IntegerField()
+    profile_slug_id = models.IntegerField()
+    manager_on_premises_user_principal_name = models.CharField(max_length=250)
+    role_category = models.CharField(max_length=100, choices=ROLES)
+
