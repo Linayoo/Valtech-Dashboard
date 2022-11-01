935ec6156475ce2729f4b68a8adc9baf4dae717e
Eugene Samarin <gekasamarin1@gmail.com>
2022-11-01 15:29:32 +0100

    LOGIN TOKEN

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/Login/Login';
-
+import SideNav from './components/SideNav/SideNav';
 function App() {
   return (
     <BrowserRouter>
       <main>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="nav" element={<SideNav />} />
         </Routes>
       </main>
     </BrowserRouter>
--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,36 +1,36 @@
-import { LoginMain, LogoString, LoginBg, LoginRemember, LoginForm, LoginBar, LoginButton, LoginLeft, LoginRight, AboutText, WelcomeLog, LoginForget } from "./login-styles"
-import useNavigate from 'react-router-dom';
+import { LoginMain, LogoString, LoginRemember, LoginForm, LoginBar, LoginButton, LoginForget } from "./login-styles"
+import { useNavigate } from 'react-router-dom';
 import { useState } from "react";
 
 function Login() {
 
-    // const navigate = useNavigate();
-    // const [email, setEmail] = useState()
-    // const [password, setPassword] = useState()
+    const navigate = useNavigate();
+    const [username, setUsername] = useState()
+    const [password, setPassword] = useState()
 
-    // const post = "POST"
-    // const header = new Headers({
-    //     "content-type": "application/json"
-    // })
+    const post = "POST"
+    const header = new Headers({
+        "content-type": "application/json"
+    })
 
-    // const body = JSON.stringify({
-    //     "email": `${email}`,
-    //     "password": `${password}`, 
-    // })
+    const body = JSON.stringify({
+        "username": `${username}`,
+        "password": `${password}`, 
+    })
 
-    // const postconfig = {
-    //     method: post,
-    //     headers: header,
-    //     body: body
-    // }
+    const postconfig = {
+        method: post,
+        headers: header,
+        body: body
+    }
 
-    // const handleSubmit = (event) => {
-    //     event.preventDefault();
-    //     fetch("https://team3luna.propulsion-learn.ch/api/auth/token/", postconfig)
-    //         .then(response => response.json())
-    //         .then((data) => {localStorage.setItem("luna-auth", JSON.stringify(data.access)); navigate('../../') })
-    //         .catch(error => console.log(error))
-    // }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch("http://localhost:8000/token/", postconfig)
+            .then(response => response.json())
+            .then((data) => {localStorage.setItem("valtech-auth", JSON.stringify(data.access)); navigate('nav/') })
+            .catch(error => console.log(error))
+    }
 
     return(
         <LoginMain>
@@ -41,15 +41,15 @@ function Login() {
             <LogoString>
                 Valtech_
             </LogoString>
-            <LoginForm>
-                <input type='text' placeholder="Email"></input>
-                <input type='text' placeholder="Password"></input>
+            <LoginForm id='log' onSubmit={handleSubmit}>
+                <input form="log" type="text" name="email" placeholder="Email" onChange={(e) => setUsername(e.target.value)}></input>
+                <input form="log" type="text" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
             </LoginForm>
             <LoginRemember>
                 <input type="checkbox"></input>
                 <p>Remember me</p>
             </LoginRemember>
-            <LoginButton>
+            <LoginButton form="log" type="submit">
                 <p>Login</p>
             </LoginButton>
             <LoginForget>
