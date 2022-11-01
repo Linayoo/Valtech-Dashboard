b3bb586766c0f8906f419c75631b081e46dfdbaf
Miri Yoo <violinmiriu@gmail.com>
2022-11-01 15:31:11 +0100

    User_Project Model modified, TimeFrame model added

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/consultants/', include('consultant.urls')),
