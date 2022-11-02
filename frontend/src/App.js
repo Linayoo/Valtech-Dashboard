9381a3d24aaa0dd6a1042b4baf822c626546b55f
Eugene Samarin <gekasamarin1@gmail.com>
2022-11-02 12:38:20 +0100

    Login autofield

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
-import './App.css';
+// import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/Login/Login';
-import SideNav from './components/SideNav/SideNav';
 import HomePage from './pages/home/home-page';
 
 
