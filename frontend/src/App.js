0fa3a8901ac0b8fc288d8d5528bf80ba70a698a1
Eugene Samarin <gekasamarin1@gmail.com>
2022-11-03 16:06:03 +0100

    Profile Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './components/Login/Login';
 import HomePage from './pages/home/home-page';
 import ProjectsPage from "./pages/projects/projects-page";
+import ProfilePage from "./pages/profile/profile-page";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="home/" element={<HomePage />} />
           <Route path="projects/" element={<ProjectsPage />} />
+          <Route path="profile/" element={<ProfilePage />} />
         </Routes>
       </main>
     </BrowserRouter>
