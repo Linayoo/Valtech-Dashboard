fa9354d8c91beeae829d6909f6c3b35f4f3dd435
Miri Yoo <violinmiriu@gmail.com>
2022-11-04 11:35:33 +0100

    User data more readable

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,5 +1,29 @@
 from django.contrib import admin
-from .models import User
+from consultant.models import Consultant
+from user.models import User
 
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    readonly_fields = ()
+
+    # fields shown when creating a new instance
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('first_name', 'last_name', 'username', 'password', 'password2')
+        }),
+    )
+
+    # fields when reading / updating an instance
+    field_sets = (
+        (None, {'fields': ('first_name', 'last_name', 'username')}),
+        ('Location', {'fields': ('country', 'city', 'office_category')}),
+        ('Contacts', {'fields': 'email'}),
+        ('Personal Details', {'fields': 'image'}),
+    )
+
+    # fields which are shown when looking at a list of instances
+    list_display = ('id', 'first_name', 'last_name')
+    ordering = ('-id', )
+
