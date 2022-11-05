36b34e4bd72bacbf7afdcd76d582fd7e78578182
Miri Yoo <violinmiriu@gmail.com>
2022-11-05 13:25:26 +0100

    modified test functions

--- backend/test/consultant/test_urls.py
+++ backend/test/consultant/test_urls.py
@@ -1,26 +1,24 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 
+from consultant.views import GetAllConsultants, CreateConsultant, RetrievePatchDeleteConsultant, SearchConsultant
+
 
 class TestConsultantUrls(SimpleTestCase):
 
     def test_get_all_consultants_is_resolved(self):
         url = reverse('get_all_consultants')
-        resolver = resolve(url)
-        self.assertEqual(resolver.view_name, 'get_all_consultants')
+        self.assertEqual(resolve(url).func.view_class, GetAllConsultants)
 
     def test_create_consultants_is_resolved(self):
         url = reverse('create_consultants')
-        resolver = resolve(url)
-        self.assertEqual(resolver.view_name, 'create_consultants')
+        self.assertEqual(resolve(url).func.view_class, CreateConsultant)
 
     def test_get_patch_delete_consultants_is_resolved(self):
         random_number = '1'  # <int:id> from url
-        url = reverse('get_patch_delete_consultants', args=random_number)
-        resolver = resolve(url)
-        self.assertEqual(resolver.view_name, 'get_patch_delete_consultants')
+        url = reverse('get_patch_delete_consultants', args=[random_number])
+        self.assertEqual(resolve(url).func.view_class, RetrievePatchDeleteConsultant)
 
     def test_search_consultants_is_resolved(self):
         url = reverse('search_consultants')
-        resolver = resolve(url)
-        self.assertEqual(resolver.view_name, 'search_consultants')
+        self.assertEqual(resolve(url).func.view_class, SearchConsultant)
