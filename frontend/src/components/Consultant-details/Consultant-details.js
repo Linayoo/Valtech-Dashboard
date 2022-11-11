2d01722c7281932dac3a38a77e7358d21374c25a
Lauraa1003 <88458945+Lauraa1003@users.noreply.github.com>
2022-11-11 15:24:29 +0100

    created 'create consultant' page and 'consultant details'

--- frontend/src/components/Consultant-details/Consultant-details.js
+++ frontend/src/components/Consultant-details/Consultant-details.js
@@ -1,4 +1,4 @@
-import { ConsultantDetailsWrapper, HeaderStyle, ConsultantStyle } from "./Consultant-details.styles"
+import { ConsultantDetailsWrapper, HeaderStyle, ConsultantStyle, FlexColumn } from "./Consultant-details.styles"
 import { useNavigate } from "react-router"
 
 
@@ -11,36 +11,112 @@ const ConsultantDetails = () => {
             <h1>Consultant details</h1>
             <button onClick={() => navigate(`/consultant/edit`)}>Edit details</button>
             </HeaderStyle>
-            <hr></hr>
-            <div>
-                <p>Image</p>
-                {/* <img src={projects.image}/> */}
+            <hr/>
             </div>
-            <div>
-                <p>Name</p>
-                <p></p>
-            </div>
-            <div>
-                <p>Description </p>
-                <p></p>
-            </div>
-            <div>
-                <p>External link</p>
-                <p></p>
-            </div>
-            <div>
-                <p>Project duration</p>
-                <p></p>
-            </div>
-            <div>
-                <p>Amount of consultants <br/> working on project</p>
-                <p></p>
-            </div>
-            <div>
-                <p>Tools used</p>
-            </div>
-        
-            </div>
+                <div>
+
+                    {/* first column */}
+
+                    <FlexColumn>
+                        <div>
+                                <p>Image</p>
+                                <img src=""/>
+                        </div>
+                        <div>
+                            <p>Display Name </p>
+                            <p></p>
+                        </div>
+                        <div>
+                            <p>First Name </p>
+                            <p></p>
+                        </div>
+                        <div>
+                            <p>Last Name </p>
+                            <p></p>
+                        </div>
+
+                        <div>
+                            <p>Username </p>
+                            <p></p>
+                        </div>
+                        <div>
+                            <p>Title</p>
+                            <p></p>
+                        </div>
+                        <div>
+                            <p>Category </p>
+                            <p></p>
+                        </div>
+                    </FlexColumn>
+
+                    {/* second column */}  
+
+                    <FlexColumn>
+                    
+                        <div>
+                            <p>Country </p>
+                            <p></p>
+                        </div>
+                        <div>
+                            <p>City </p>
+                            <p></p>
+                        </div>
+                        <div>
+                            <p>Office </p>
+                            <p></p>
+                        </div>
+                        <div>
+                            <p>Manager</p>
+                            <p></p>
+                        </div>
+                    
+                        <div>
+                            <p>LinkedIn</p>
+                            <p></p>
+                        </div>
+                        <div>
+                            <p>Email</p>
+                            <p></p>
+                        </div>
+                        <div>
+                            <p>Summary</p>
+                            <p></p>
+                        </div>
+                    </FlexColumn>
+
+                     {/* third column */}
+
+                    <FlexColumn>
+                       <div>
+                            <p>Primary Language</p>
+                    
+                        </div>
+                        <div>
+                            <p>Language Skills</p>
+                            
+                        </div>
+                        <div>
+                            <p>Skills</p>
+                        
+                        </div>
+                        <div>
+                            <p>Additional Skills</p>
+                        
+                        </div>
+                        <div>
+                            <p>Educations</p>
+                            
+                        </div>
+                        <div>
+                            <p>Certificate</p>
+                        
+                        </div>
+                        <div>
+                            <p>Unavailable</p>
+
+                        </div>
+                    </FlexColumn>
+                </div>
             
         </ConsultantDetailsWrapper>
     )
