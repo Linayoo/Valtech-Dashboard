d0c11293be81d4c89459ba05c2915bbe8b5992ff
Lauraa1003 <88458945+Lauraa1003@users.noreply.github.com>
2022-11-14 20:14:01 +0100

    header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,12 +1,36 @@
 import {BsSearch, BsBell, BsChevronDown} from "react-icons/bs";
 import { HeaderContainer } from "./Header.styles";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router";
 
 
 const Header = () => {
+    let localToken = localStorage.getItem("valtech-auth")
+    const navigate = useNavigate()
+
+    const [currentUser, setCurrentUser] = useState([])
+
+    const get = "GET"
+    const header = new Headers({
+        "Authorization": `Bearer ${localToken}`,
+        "content-type": "application/json",
+    })
+
+    const getconfig = {
+    method: get,
+    headers: header,
+    }
+
+useEffect((state) => {
+    fetch("http://localhost:8000/api/me/", getconfig)
+        .then(response => response.json())
+        .then(data => {console.log(data); setCurrentUser(data)})
+        .catch(error => console.log(error));
+}, [])
     return (
         <HeaderContainer>
-            <img src="/images/patricia.png"/>
-            <p>Jane Doe </p>
+            {/* <img src={currentUser.image}/> */}
+            <p>{`${currentUser.first_name} ${currentUser.last_name}`} </p>
             <BsChevronDown size={22} color={"#1b253d"} />
             <button ><BsBell size={22} color={"#5a5757"}/></button>
         </HeaderContainer>
