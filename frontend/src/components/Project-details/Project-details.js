4edcd6b256498e9c83f7f818e7294e816c047323
Miri Yoo <violinmiriu@gmail.com>
2022-11-15 11:27:01 +0100

    project detail background image fixed

--- frontend/src/components/Project-details/Project-details.js
+++ frontend/src/components/Project-details/Project-details.js
@@ -63,7 +63,7 @@ const ProjectDetails = () => {
     },[])
 
     return (
-        <ProjectDetailsWrapper background={`http://localhost:8000${project?.image}`}>
+        <ProjectDetailsWrapper background={`${project?.image}`}>
             <div>
             <HeaderStyle>
             <h1>Project details</h1>
