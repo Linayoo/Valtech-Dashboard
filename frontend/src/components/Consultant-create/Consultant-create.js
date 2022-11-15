b1048cada6ec3c53e44fa2cbed7f765454bbaa6d
leventepap <pap.levente.i@gmail.com>
2022-11-15 20:49:35 +0100

    widgets finish plus django debug

--- frontend/src/components/Consultant-create/Consultant-create.js
+++ frontend/src/components/Consultant-create/Consultant-create.js
@@ -1,32 +1,39 @@
 import { CreateConsultantContainer, } from "./Consultant-create.styles";
 import React, { useState } from "react";
+import { useEffect } from "react"
+import { useRef } from "react"
+import ToolAddTag from "../Project-edit/Tool-add-tag";
+import ToolTag from "../Project-edit/Tool-tag";
 
 const CreateConsultant = () => {
 
     let localToken = localStorage.getItem("valtech-auth")
-
+    const inputref = useRef([])
 
     const [displayName, setDisplayName] = useState("")
     const [firstname, setFirstname] = useState("")
     const [lastname, setLastname] = useState("")
     const [title, setTitle] = useState("")
-    const [username, setUsername] = useState("")
     const [country, setCountry] = useState("")
     const [city, setCity] = useState("")
     const [officeCat, setOfficeCat] = useState("")
     const [email, setEmail] = useState("")
     const [linkedIn, setLinkedIn] = useState("")
-    const [summary, setSummary] = useState("")
     const [prmLang, setPrmLang] = useState("")
     const [manager, setManager] = useState("")
     const [role, setRole] = useState("")
     const [langSkill, setLangSkill] = useState("")
     const [skill, setSkill] = useState("")
     const [addSkill, setAddSkill] = useState("")
-    const [certificate, setCertificate] = useState("")
+    const [image, setImage] = useState("")
+    const [alltools, setAlltools] = useState()
+    const [tools, setTools] = useState([])
+    const [toolsresults, setToolsresults] = useState()
+    /*const [certificate, setCertificate] = useState("")
     const [unavailable, setUnavailable] = useState("")
     const [education, setEducation] = useState("")
-    const [image, setImage] = useState("")
+    const [username, setUsername] = useState("")
+    const [summary, setSummary] = useState("")*/
 
     const handleUpload = e => {
         const imageUrl = e.target.files;
@@ -36,6 +43,7 @@ const CreateConsultant = () => {
 
     const post = "POST"
     const patch = "PATCH"
+    const get = "GET"
 
     const headers = new Headers({
         "Authorization": `Bearer ${localToken}`,
@@ -44,34 +52,48 @@ const CreateConsultant = () => {
 
     const fileUploadHeaders = new Headers({
         "Authorization": `Bearer ${localToken}`,
-        // "content-type": "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
     })
 
-    const parseStringToList = (inputString) => { return inputString.split(",").map(Number)} 
+    const parseStringToList = (inputString) => { return inputString.split(",").map(Number) }
 
-    const consultant = JSON.stringify({
-        "display_name": `${displayName}`,
-        "first_name": `${firstname}`,
-        "last_name": `${lastname}`,
-        "title": `${title}`,
-        "country": `${country}`,
-        "city": `${city}`,
-        "office_category": `${officeCat}`,
-        "email": `${email}`,
-        "linked_in_link": `${linkedIn}`,
-        "primary_language": `${prmLang}`,
-        "language_skills": parseStringToList(langSkill),
-        "managed_skills": parseStringToList(skill),
-        "addition_skills": parseStringToList(addSkill),
-        "manager_display_name": `${manager}`,
-        "role_category": `${role}`,
+    const body = () => {
 
-    })
+        let newtoolsArray = [...tools]
+        let toolsmagic = ""
+        newtoolsArray.forEach(e => toolsmagic = `${toolsmagic}${e.id},`)
+        toolsmagic = toolsmagic.slice(0, -1)
+        toolsmagic = parseStringToList(toolsmagic)
+
+        if (toolsmagic.includes(NaN)) {
+            toolsmagic = []
+        }
+
+        let fetchbody = JSON.stringify({
+            "display_name": `${displayName}`,
+            "first_name": `${firstname}`,
+            "last_name": `${lastname}`,
+            "title": `${title}`,
+            "country": `${country}`,
+            "city": `${city}`,
+            "office_category": `${officeCat}`,
+            "email": `${email}`,
+            "linked_in_link": `${linkedIn}`,
+            "primary_language": `${prmLang}`,
+            "language_skills": parseStringToList(langSkill),
+            "managed_skills": toolsmagic,
+            "addition_skills": parseStringToList(addSkill),
+            "manager_display_name": `${manager}`,
+            "role_category": `${role}`,
+
+        })
+        return fetchbody
+
+    }
 
     const config = {
         method: post,
         headers: headers,
-        body: consultant,
+        body: body(),
     }
 
     const imageData = new FormData()
@@ -83,18 +105,60 @@ const CreateConsultant = () => {
         body: imageData
     }
 
+    const getconfig = {
+        method: get,
+        headers: headers
+    }
+
+    useEffect((state) => {
+        fetch(`https://valtech-dashboard.propulsion-learn.ch/backend/api/skills/`, getconfig)
+            .then(response => response.json())
+            .then(data => setAlltools(data))
+            .catch(error => console.log(error));
+    }, [])
+
     const createConsultant = (event) => {
         event.preventDefault();
         console.log(config.body)
-        fetch(`https://valtech-dashboard.propulsion-learn.ch/backend/api/new/`, config)
-        .then(response => response.json())
-        .then((data) => fetch(`https://valtech-dashboard.propulsion-learn.ch/backend/api/consultants/${data.id}/`, imageUploadConfig))
-        .then(response => response.json())
-        .then((data) => console.log(data))
-        .catch(error => console.log(error));
+        fetch(`https://valtech-dashboard.propulsion-learn.ch/backend/api/consultants/new/`, config)
+            .then(response => response.json())
+            .then((data) => fetch(`https://valtech-dashboard.propulsion-learn.ch/backend/api/consultants/${data.id}/`, imageUploadConfig))
+            .then(response => response.json())
+            .then((data) => console.log(data))
+            .catch(error => console.log(error));
     }
 
-    
+    const handleToolFilter = () => {
+        const query = {
+            tools: inputref.current.tools.value,
+        }
+        let updatedList = [...alltools];
+        updatedList = updatedList.filter(element =>
+            element.title.toLowerCase().indexOf(query.tools.toLowerCase()) !== -1 &&
+            !JSON.stringify(tools).includes(JSON.stringify(element))
+        )
+        if (query.tools === "") {
+            setToolsresults([])
+        } else {
+            setToolsresults(updatedList)
+        }
+    }
+
+    const handleAddTool = (event) => {
+        event.preventDefault()
+        let newArray = [...tools]
+        newArray.push(JSON.parse(event.target.id))
+        setTools(newArray)
+        inputref.current.tools.value = ""
+        handleToolFilter()
+    }
+
+    const handleDeleteTool = (event) => {
+        event.preventDefault()
+        let newArray = [...tools]
+        newArray.splice(event.target.id, 1);
+        setTools(newArray)
+    }
 
     return (
         <CreateConsultantContainer>
@@ -103,7 +167,7 @@ const CreateConsultant = () => {
             </div>
 
             <form>
-                
+
                 <div>
                     <label>
                         <p>Display Name *</p>
@@ -112,6 +176,8 @@ const CreateConsultant = () => {
                     <label>
                         <p>First Name *</p>
                         <input type="text" name="first_name" onChange={e => setFirstname(e.target.value)} required></input>
+                    </label>
+                    <label>    
                         <p>Last Name *</p>
                         <input type="text" name="last_name" onChange={e => setLastname(e.target.value)} required></input>
                     </label>
@@ -127,16 +193,25 @@ const CreateConsultant = () => {
                             <option value="backend">Backend Developer</option>
                             <option value="fullstack">Full Stack Developer</option>
                             <option value="devops">DevOps Developer</option>
+                            <option value="manager">Manager</option>
                         </select>
                     </label>
-                
                     <label>
-                        <p>Country *</p>
-                        <input name="country" onChange={e => setCountry(e.target.value)} required>
-                        </input>
-                    </label>
+                    <p>Tools</p>
+                    <div className="tools-search">
+                    <div className="tools-child">
+                        <input className="tools-input" ref={ref => inputref.current.tools = ref} type='text' placeholder='Search for tools...' onChange={handleToolFilter} />
+                        </div>
+                        <div className="tools-tags">
+                        {toolsresults === undefined ? <></> : toolsresults.map((element, index) => <ToolAddTag element={element} add={handleAddTool} />)}
                     </div>
-                    <div>
+                    </div>
+                    <div className="tools-onproject">
+                        {tools === undefined ? "" : tools.map((element, index) => <ToolTag id={index} tool={element} remove={handleDeleteTool} />)}
+                    </div>
+                    </label>
+                </div>
+                <div className="test">
                     <label>
                         <p>City *</p>
                         <input type="text" name="city" onChange={e => setCity(e.target.value)} required></input>
@@ -155,7 +230,7 @@ const CreateConsultant = () => {
                         <p>Manager</p>
                         <input type="text" name="manager_display_name" onChange={e => setManager(e.target.value)}></input>
                     </label>
-                
+
                     <label>
                         <p>LinkedIn</p>
                         <input type="url" name="linked_in_link" onChange={e => setLinkedIn(e.target.value)}></input>
@@ -164,23 +239,24 @@ const CreateConsultant = () => {
                         <p>Email</p>
                         <input type="email" name="email" onChange={e => setEmail(e.target.value)}></input>
                     </label>
-                    <label>
-                        <p>Primary Language</p>
-                        <input type="text" name="primary_language" onChange={e => setPrmLang(e.target.value)}></input>
-                    </label>
-                    </div>
-                    <div>
+                </div>
+                <div className="test">
                     <label>
                         <p>Language Skills</p>
                         <input type="text" name="language_skills" onChange={e => setLangSkill(e.target.value)}></input>
                     </label>
                     <label>
-                        <p>Skills</p>
-                        <input type="text" name="managed_skills" onChange={e => setSkill(e.target.value)}></input>
+                        <p>Country *</p>
+                        <input name="country" onChange={e => setCountry(e.target.value)} required>
+                        </input>
                     </label>
                     <label>
                         <p>Additional Skills</p>
                         <input type="text" name="addition_skills" onChange={e => setAddSkill(e.target.value)}></input>
+                    </label>
+                    <label>
+                        <p>Primary Language</p>
+                        <input type="text" name="primary_language" onChange={e => setPrmLang(e.target.value)}></input>
                     </label>
                     <div>
                         <label>
@@ -191,9 +267,16 @@ const CreateConsultant = () => {
                 </div>
             </form>
             <div>
-                    <button type={"submit"} onClick={createConsultant}>SUBMIT</button>
+                <button type={"submit"} onClick={createConsultant}>SUBMIT</button>
             </div>
         </CreateConsultantContainer>
     )
 }
 export default CreateConsultant;
+
+/*
+<label>
+<p>Skills</p>
+<input type="text" name="managed_skills" onChange={e => setSkill(e.target.value)}></input>
+</label>
+*/
